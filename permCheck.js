6d4191ddfa8034fd38734b92c8cc6dc9a2b2b6c0
Daria Tkachuk <daryasmail@gmail.com>
2018-07-18 14:01:43 +0300

    PermCheck Task

---
+++ permCheck.js
@@ -0,0 +1,65 @@
+
+/* TASK DESCRIPTION
+A non-empty array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+is a permutation, but array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+function solution(A);
+
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+the function should return 1.
+
+Given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+the function should return 0.
+*/
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    var n = A.length;
+    
+    A.sort(function(a, b){
+      return a - b;  
+    });
+    
+    
+    if (A[0] != 1) {
+        return 0;
+    }
+    
+    for (var i = 1; i < n; i++) {
+        if (A[i] - A[i-1] != 1) {
+            return 0;
+        }
+    }
+    return 1;
+}
